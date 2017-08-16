2c0f85607ca4710edb04c4c39a6da8530c23308a
agudi24 <league@WorthyMini-6.attlocal.net>
2017-08-16 15:00:03 -0700

    Day 3

--- src/day3/SpeakAndSpell.java
+++ src/day3/SpeakAndSpell.java
@@ -12,16 +12,27 @@ import javax.swing.JOptionPane;
  **/
 
 public class SpeakAndSpell {
+static	String word = "marvelous";
+
 
 	public static void main(String[] args) {
 		// 1. Use the speak method to say the word. "e.g. spell mandlebrot"
+		
+		
+		speak("marvelous");
+		
+		String word = JOptionPane.showInputDialog("Spell this word: (Use lowercase)");
+		
 
 		// 2. Catch the user's answer in a String
+		if(word.equals("marvelous")) {
+			JOptionPane.showMessageDialog(null,"Correct!, you are awesome!");
+		}
 
 		// 3. If the user spelled the word correctly, speak "correct"
-
+		else { JOptionPane.showMessageDialog(null, word + " is incorrect. Try again.");
 		// 4. Otherwise say "wrong"
-
+		}
 		// 5. repeat the process for other words
 		
 	}
